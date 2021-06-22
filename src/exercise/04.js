0b1bc80251b5cc6533263971e38054ae343fe9ae
Chirag Jain <chirgjn@users.noreply.github.com>
2021-06-23 03:49:46 +0530

    solutions

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -7,29 +7,39 @@ import {Switch} from '../switch'
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
+  const getTogglerProps = ({onClick, ...otherProps}) => ({
+    onClick: () => {
+      toggle()
+      if (typeof onClick === 'function') {
+        onClick()
+      }
+    },
+    'aria-pressed': on,
+    ...otherProps
+  })
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  return {on, toggle, getTogglerProps};
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
   )
 }
 
+
 export default App
 
-/*
-eslint
-  no-unused-vars: "off",
-*/
